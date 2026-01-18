cad0aac1a5cfb8919e5074c22a71246040b04fa6
arttutolli <arttu.to@hotmail.com>
2026-01-18 14:27:56 +0200

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -1,42 +1,12 @@
 #include <iostream>
-#include <cstdlib>
-#include <ctime>
-
+#include "Game.h"
 using namespace std;
 
-int pelaaPeli(int maksimiLuku) {
-
-    time_t nykyAika = time(nullptr);
-    srand(nykyAika);
-
-    int salainenLuku = rand() % maksimiLuku;
-    int pelaajanArvaus = -1;
-    int arvaustenMaara = 0;
-
-    while (pelaajanArvaus != salainenLuku) {
-        cout << "Arvaa lukua: ";
-        cin >> pelaajanArvaus;
-        arvaustenMaara++;
-
-        if (pelaajanArvaus < salainenLuku) {
-            cout << "Luku on isompi" << endl;
-        }
-        else if (pelaajanArvaus > salainenLuku) {
-            cout << "Luku on pienempi" << endl;
-        }
-    }
-
-    cout << "Oikein arvattu!" << endl;
-    return arvaustenMaara;
-}
-
 int main() {
-    cout << "Anna maksimi luku minka kone pystyy valitsemaan: ";
-    int maksimiLuku;
-    cin >> maksimiLuku;
+    Game peli(10);
 
-    int arvaustenMaara = pelaaPeli(maksimiLuku);
+    peli.Play();
+    peli.PrintGameResult();
 
-    cout << "Arvauksia yhteensa: " << arvaustenMaara << endl;
     return 0;
 }
